f65a3130d379eade63e2ec3d225296b61ed14263
2% <tusamss@gmail.com>
2018-05-28 23:24:17 +0800

    指向非常數資料的非常數指標

---
+++ bubbleSort.c
@@ -0,0 +1,24 @@
+void swap(int *element1Ptr,int *element2Ptr); // 數值互換副程式
+
+void bubbleSort( int  * array ,int size)       //非常數指標  氣泡排序副程式 
+{
+ int j,n;
+ int count;
+ for(count=0;count<size-1;count++)
+ {
+  for(j=0;j<size-1;j++)
+  {
+   if (array[j]>array[j+1])
+   {
+    swap(&array[j],&array[j+1]);
+   }
+  }
+ }
+}
+
+void swap(int *element1Ptr,int *element2Ptr)
+{
+ int hold= *element1Ptr;
+ *element1Ptr=*element2Ptr;
+ *element2Ptr=hold;
+}
---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#define SIZE 10
+
+void bubbleSort( int  *  array, int size);    //非常數指標  氣泡排序副程式  
+ 
+int main(void)
+{
+   int a[10]={2,5,9,7,4,6,3,0,8,1,6};  //非常數資料 
+   int i,size;
+   
+   puts("資料排序前");
+   
+    for(i=0;i<=9;i++)
+    {
+     printf("%d ",a[i]);
+    }
+    bubbleSort( a, 10);
+    
+ puts("\n資料排序後");
+ for(i=0;i<=9;i++)
+ {
+  printf("%d ",a[i]);
+ }
+}
+
+
